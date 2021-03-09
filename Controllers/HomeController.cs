41ee6337b6350d4c907d8f62992d9942251e2092
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-03-10 00:17:05 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Alpha_blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alpha_blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alpha_blog.Models
+{
+    public class Post
+    {
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
