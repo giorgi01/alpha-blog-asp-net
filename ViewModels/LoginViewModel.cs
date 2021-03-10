b11de953c4a644223aa8d3de156251fd1274c30b
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2021-03-10 16:00:13 +0400

    Add AuthController

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alpha_blog.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
